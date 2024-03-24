34ab2465f62473fe78799b993bd0fbfa88283239
KadnNeal <k.s.neal04@gmail.com>
2024-03-23 20:56:02 -0700

    mobile layout update

--- p.js
+++ p.js
@@ -1,14 +1,11 @@
-// Step 1: Select the div by its ID or class
-var clickableDiv = document.getElementById('swe-portfolio-front');
+var swePortfolio = document.getElementById('swe-portfolio-front');
 
-// Step 2: Add a click event listener to the div
-clickableDiv.addEventListener('click', function() {
-    // Step 3: Change the location of the window to navigate to the new page
+swePortfolio.addEventListener('click', function() {
     window.location.href = 'swe-portfolio.html';
 });
 
-const homeButton = document.getElementById('homeButton');
-homeButton.addEventListener('click', function(e) {
-    window.location.href = 'index.html';
-    return false;
-})
+var fePortfolio = document.getElementById('field-tech-portfolio-front');
+
+fePortfolio.addEventListener('click', function() {
+    window.location.href = 'fe-portfolio.html';
+}); 
